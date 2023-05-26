3194a86322da6916e58569fd3aae03e6d662e903
Bishal Tamang <tbishal078@gmail.com>
2023-05-26 10:21:45 +0545

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
+import { CartContext } from '../contexts/CartContext';
+
+import {BsBag} from 'react-icons/bs'
+
+import Logo from '../img/logo.svg'
+
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+//header state
+  const [isActive, setIsActive] = useState(false);
+ const { isOpen, setIsOpen } = useContext(SidebarContext);
+ const { itemAmount } = useContext(CartContext);
+
+ //event listener
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+
+  return (
+    <header className= {`${isActive ? 'bg-white py-2 shadow-md' : 'bg-none py-4'} fixed w-full z-10 transition-all`}>
+
+    <div className='container mx-auto flex items-center justify-between h-full'>
+
+      {/*Logo*/}
+      <Link to={'/'}>
+        <div>
+          <img className='w-[40px] ' src={Logo} alt="" />
+        </div>
+      </Link>
+
+      {/*Cart*/}
+     
+      <div onClick={()=> setIsOpen(!isOpen)}
+      className='cursor-pointer flex relative'> 
+        <BsBag className='text-2xl'/> 
+        <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center'>{itemAmount}</div>
+      </div>
+    </div>
+  </header>
+  )
 };
 
 export default Header;
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,104 @@
-import React from 'react';
+import React,{createContext, useState, useEffect} from 'react';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+export const CartContext = createContext()
+
+const CartProvider = ({children}) => {
+  //cart state
+  const [cart, setCart] = useState([]);
+  
+  //item amount state
+  const [itemAmount, setItemAmount] = useState(0); 
+
+  //total price state
+  const [total, setTotal] = useState(0);
+
+  useEffect(()=>{
+    const total = cart.reduce((acc, currentItem)=>{
+      return acc + currentItem.price * currentItem.amount;
+    }, 0)
+    setTotal(total);
+  });
+
+//update item amount
+  useEffect(()=>{
+    if(cart) {
+      const amount = cart.reduce((accumulator, currentItem) =>{
+        return accumulator + currentItem.amount;
+      }, 0);
+      setItemAmount(amount);
+    }
+  }, [cart]);
+
+  //add to cart
+  const addToCart= (product, id)=>{
+
+    const newItem = { ...product, amount: 1};
+    //checking if the item already exits in the cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    console.log(cartItem);
+    //if cart item already exists in the cart
+    if(cartItem){
+      const newCart = [...cart].map(item =>{
+        if(item.id === id) {
+          return {
+            ...item, amount: cartItem.amount + 1
+          };
+        }else{
+          return item;
+        }
+      });
+      setCart(newCart);
+    }else{
+      setCart([...cart, newItem]);
+    }
+  
+  };
+
+  //remove from cart
+  const removeFromCart = id =>{
+    const newCart = cart.filter(item=>{
+      return item.id !== id;
+    });
+    setCart(newCart);
+  }
+
+  //clear cart
+  const clearCart = ()=>{
+    setCart([]);
+  }
+
+//increase amount
+const increasedAmount = (id) =>{
+  const CartItem = cart.find(item => item.id === id);
+  addToCart(CartItem, id);
+  
+}
+
+//decrease amount
+const decreaseAmount = (id) =>{
+  const CartItem = cart.find(item=> item.id === id);
+ if(CartItem){
+  const newCart = cart.map(item =>{
+    if(item.id === id){
+      return {...item, amount: CartItem.amount - 1};
+    }else{
+      return item;
+    }
+  });
+  setCart(newCart);
+ }
+
+  if(CartItem.amount < 2){
+    removeFromCart(id);
+  }
+ 
+}
+
+  return <CartContext.Provider value={{cart, addToCart, removeFromCart, clearCart, increasedAmount, decreaseAmount, itemAmount, total}}>
+    {children}
+    </CartContext.Provider>;
 };
 
-export default CartContext;
+export default CartProvider;
